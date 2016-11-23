d1e8785e6ba8bc78ff09814dda592f78979f9240
Vanessa <v4n3ss4ms@gmail.com>
2016-11-23 14:13:22 +0100

    print()

--- bot-telegram.py
+++ bot-telegram.py
@@ -12,7 +12,7 @@ def listener(messages):
             chat_id = str(m.chat.id)
             user_name = str(m.chat.first_name)
             user_text = m.text
-            print user_name + " [" + chat_id + "]: " + user_text
+            print (user_name + " [" + chat_id + "]: " + user_text)
 
 bot.set_update_listener(listener)  # register listener
 
